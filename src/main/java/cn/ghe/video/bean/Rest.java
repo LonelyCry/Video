e9369bd6323cf7224e7b7dfbc5ba9b32c88e8dbd
guohe <1742691741@qq.com>
2020-08-17 22:36:10 +0800

    three

--- src/main/java/cn/ghe/video/bean/Rest.java
+++ src/main/java/cn/ghe/video/bean/Rest.java
@@ -1,10 +1,46 @@
 package cn.ghe.video.bean;
 
+import java.util.List;
+
 /**
  * 返回给前端的结果
  */
 public class Rest {
+
+    //登录成功返回值
     private String status;
+
+    //返回信息：登录成功、上传成功
+    private String msg;
+
+    //状态：true/false
+    private Boolean success;
+
+    //信息列表
+    private List list;
+
+    //所有数据
+    private int total;
+
+    //分页条数
+    private int pageSize;
+
+    //当前页面
+    private int page;
+
+    //视频预览时，返回的新list
+    private List videoList;
+
+    //判断用户权限：admin:登录成功，guest:登录失败
+    private String currentAuthority;
+
+    public String getCurrentAuthority() {
+        return currentAuthority;
+    }
+
+    public void setCurrentAuthority(String currentAuthority) {
+        this.currentAuthority = currentAuthority;
+    }
 
     public String getStatus() {
         return status;
@@ -12,5 +48,61 @@ public class Rest {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List getVideoList() {
+        return videoList;
+    }
+
+    public void setVideoList(List videoList) {
+        this.videoList = videoList;
     }
 }
--- src/main/java/cn/ghe/video/controller/LoginController.java
+++ src/main/java/cn/ghe/video/controller/LoginController.java
@@ -1,9 +1,12 @@
 package cn.ghe.video.controller;
 
+import cn.ghe.video.bean.LoginDO;
 import cn.ghe.video.bean.Rest;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,11 +18,22 @@ public class LoginController {
 
     @RequestMapping("/login")
     @ResponseBody
+    @ApiImplicitParam(name = "LoginDO",value = "测试",required = true,dataType = "LoginDO")
     @ApiOperation(value = "登陆" , notes = "用户登陆")
-    public String index() {
+    public Rest index(@RequestBody LoginDO loginDo) {
         Rest mv = new Rest();
-        mv.setStatus("ok");
-        return "成功";
+        if("admin".equals(loginDo.getUserName())){
+            mv.setStatus("ok");
+            mv.setSuccess(true);
+            mv.setMsg("登录成功!");
+            mv.setCurrentAuthority("admin");
+        }else{
+            mv.setStatus("error");
+            mv.setSuccess(false);
+            mv.setMsg("登录失败!");
+            mv.setCurrentAuthority("guest");
+        }
+        return mv;
 
     }
 }
